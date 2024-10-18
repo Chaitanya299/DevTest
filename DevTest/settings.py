3bc4eb88e9cfe13b6caa4ce91411fe53f8b8a0a8
Chaitanya299 <92148748+Chaitanya299@users.noreply.github.com>
2024-10-18 18:28:44 +0530

    fixed allowed hosts

--- DevTest/settings.py
+++ DevTest/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 import os
 from pathlib import Path
 
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -24,9 +26,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3+1yox0xyfvb5k148x)14+atk73zw7eudrgqhhx3pr4y6a3wv4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = 'False'
 
-ALLOWED_HOSTS = ['devtest88.herokuapp.com']
+ALLOWED_HOSTS = ['devtest88-caa4c65e5aa3.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -118,9 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
